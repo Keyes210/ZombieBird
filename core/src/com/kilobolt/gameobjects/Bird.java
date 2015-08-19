a82a454edd9bb2c942852f72bea52e3868407487
Alex Lowe <loweab@gmail.com>
2015-08-18 22:45:43 -0400

    Day 9 complete, collision detection, sounds, score display

--- core/src/com/kilobolt/gameobjects/Bird.java
+++ core/src/com/kilobolt/gameobjects/Bird.java
@@ -1,6 +1,8 @@
 package com.kilobolt.gameobjects;
 
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
+import com.kilobolt.zbhelpers.AssetLoader;
 
 /**
  * Created by Alex on 7/29/2015.
@@ -15,6 +17,10 @@ public class Bird {
     private int width;
     private int height;
 
+    private Circle boundingCircle;
+
+    private boolean isAlive;
+
     public Bird(float x, float y, int width, int height){
         this.width = width;
         this.height = height;
@@ -22,6 +28,10 @@ public class Bird {
         position = new Vector2(x,y);
         velocity = new Vector2(0,0);
         acceleration = new Vector2(0,450);
+
+        boundingCircle = new Circle();
+
+        isAlive = true;
     }
 
     public void update(float delta){
@@ -30,10 +40,54 @@ public class Bird {
         if (velocity.y > 200) velocity.y = 200;
 
         position.add(velocity.cpy().scl(delta));
+
+        // Set the circle's center to be (9, 6) with respect to the bird.
+        // Set the circle's radius to be 6.5f;
+        boundingCircle.set(position.x + 9, position.y + 6, 6.5f);
+
+        // Rotate counterclockwise
+        if(velocity.y < 0){
+            rotation -= 600 * delta;
+
+            if (rotation < -20) {
+                rotation = -20;
+            }
+        }
+
+        // Rotate clockwise
+        if(isFalling() || !isAlive){
+            rotation += 480 * delta;
+            if(rotation > 90){
+                rotation = 90;
+            }
+        }
+
+
+    }
+
+    public boolean isFalling(){
+        return velocity.y > 110;
+    }
+
+    public boolean shouldntFlap(){
+        return velocity.y > 70 || !isAlive;
     }
 
     public void onClick(){
-        velocity.y = -140;
+        if (isAlive) {
+            velocity.y = -140;
+            AssetLoader.flap.play();
+        }
+    }
+
+    public void die(){
+        isAlive = false;
+        velocity.y = 0;
+    }
+
+    public void decelerate(){
+        // We want the bird to stop accelerating downwards once it is dead.
+        acceleration.y = 0;
     }
 
     public float getX(){
@@ -54,6 +108,14 @@ public class Bird {
 
     public float getRotation(){
         return rotation;
+    }
+
+    public Circle getBoundingCircle() {
+        return boundingCircle;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
     }
 
 }
---
+++ core/src/com/kilobolt/gameobjects/Grass.java
@@ -0,0 +1,12 @@
+package com.kilobolt.gameobjects;
+
+/**
+ * Created by Alex on 8/7/2015.
+ */
+public class Grass extends Scrollable {
+
+
+    public Grass(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+    }
+}
